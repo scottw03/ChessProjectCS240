ac6460da1292f9ad2ad7b84c76723ba89d507caa
Scott Wilson <scottw03@byu.edu>
2026-06-10 13:25:35 -0600

    Commit 4 of Phase 6, the remaining command classes have been created before implementing websocket entirely

---
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -0,0 +1,12 @@
+package websocket.commands;
+
+public class ConnectCommand extends UserGameCommand {
+    public ConnectCommand(
+            String authToken,
+            Integer gameID) {
+        super(
+                CommandType.CONNECT,
+                authToken,
+                gameID);
+    }
+}
---
+++ shared/src/main/java/websocket/commands/LeaveCommand.java
@@ -0,0 +1,12 @@
+package websocket.commands;
+
+public class LeaveCommand extends UserGameCommand {
+    public LeaveCommand(
+            String authToken,
+            Integer gameID) {
+        super(
+                CommandType.LEAVE,
+                authToken,
+                gameID);
+    }
+}
